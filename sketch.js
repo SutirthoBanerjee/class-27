a3d1784d6f273827a0e72b55e7e9ddbbd969a8d8
SutirthoBanerjee <sutirthobsarkhel@gmail.com>
2021-01-28 19:11:58 +0530

    made a chain hammer

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
@@ -29,13 +29,12 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
-
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(230,180,80,PI/2)
     bird = new Bird(100,100);
-
+    chain = new Chain(bird.body,log6.body)
 }
 
 function draw(){
@@ -49,16 +48,15 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
     box3.display();
     box4.display();
     pig3.display();
     log3.display();
-
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
+    chain.display()
     bird.display();
     platform.display();
 }
